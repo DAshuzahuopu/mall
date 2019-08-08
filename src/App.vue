f7830d9c9c4be88a99e0b20e4fe91b869806a89d
Caihongyu <784356719@qq.com>
2019-08-08 17:28:14 +0800

    引入tabbar

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,17 @@
   </div>
 </template>
 
+<!--<script>-->
+  <!--import MainTabBar from 'components/content/mainTabbar/MainTabBar'-->
+
+  <!--export default {-->
+    <!--name: 'app',-->
+    <!--components: {-->
+      <!--MainTabBar-->
+    <!--}-->
+  <!--}-->
+<!--</script>-->
+
 <style lang="less">
   @import "assets/css/base.css";
 </style>
